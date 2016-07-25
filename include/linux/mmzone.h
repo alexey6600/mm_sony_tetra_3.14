5fe314a9ffe40f0d19969a8a2adea1cb22c57e8e
Alex Domnitskiy <alex@ient.net>
2016-07-25 12:44:33 +0300

    Remove UKSM

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -150,9 +150,6 @@ enum zone_stat_item {
 	NR_CMA_ACTIVE_FILE,
 	NR_CMA_UNEVICTABLE,
 	NR_CMA_CONTIG_PAGES,
-#ifdef CONFIG_UKSM
-        NR_UKSM_ZERO_PAGES,
-#endif
 	NR_VM_ZONE_STAT_ITEMS };
 
 /*
@@ -872,7 +869,7 @@ static inline int is_normal_idx(enum zone_type idx)
 }
 
 /**
- * is_highmem - helper function to quickly check if a struct zone is a
+ * is_highmem - helper function to quickly check if a struct zone is a 
  *              highmem zone or not.  This is an attempt to keep references
  *              to ZONE_{DMA/NORMAL/HIGHMEM/etc} in general code to a minimum.
  * @zone - pointer to struct zone variable
