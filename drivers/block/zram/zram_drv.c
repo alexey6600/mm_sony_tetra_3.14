d84805d232c4c7595ed271b2b708942280f4de8d
Alex Domnitskiy <alex@ient.net>
2017-03-28 22:01:24 +0300

    Zram: Use lz4hc only

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -40,11 +40,7 @@
 /* Globals */
 static int zram_major;
 static struct zram *zram_devices;
-#ifdef CONFIG_CRYPTO_LZ4HC
 static const char *default_compressor = "lz4hc";
-#else
-static const char *default_compressor = "lzo";
-#endif
 
 /*
  * We don't need to see memory allocation errors more than once every 1
